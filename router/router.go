2291163eb6b1408e3e73356d7b3c7f973efdeb3f
Suraj-Encoding <dalvisuraj256@gmail.com>
2024-10-03 14:29:01 +0530

    Refactor MongoDB connection and update import paths

--- router/router.go
+++ router/router.go
@@ -12,7 +12,7 @@ func Router() *mux.Router {
 	router.HandleFunc("/api/movie", controller.CreateMovie).Methods("POST")
 	router.HandleFunc("/api/movie/{id}", controller.MarkAsWatched).Methods("PUT")
 	router.HandleFunc("/api/movie/{id}", controller.DeleteAMovie).Methods("DELETE")
-	router.HandleFunc("/api/deleteallmovie", controller.DeleteAllMovies).Methods("DELETE")
+	router.HandleFunc("/api/dmovies", controller.DeleteAllMovies).Methods("DELETE")
 
 	return router
 }
